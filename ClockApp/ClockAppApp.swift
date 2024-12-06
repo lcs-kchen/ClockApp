6542be27206cba7a8022279924bbd7fd1e162f79
Julien Hwang <jhwang@lcs.on.ca>
2024-12-06 14:26:12 -0500

    Initial Commit

---
+++ ClockApp/ClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockAppApp.swift
+//  ClockApp
+//
+//  Created by Julien Hwang on 6/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
